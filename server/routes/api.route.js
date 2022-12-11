baddb5b6e3355c85946fc42062b7fbbbad33e4f7
Dreammmmm <49468390+PSKdream@users.noreply.github.com>
2022-12-11 23:23:02 +0700

    Update api.route.js

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const apiRoute = express.Router();
 const axios = require("axios");
-const url = "https://pskdream.pythonanywhere.com/fahp";
+const url = "https://gdr-backend.herokuapp.com/";
 
 //model
 let courseModel = require("../models/Course.js");
